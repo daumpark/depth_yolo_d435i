e8f273cd7625fa7393094edeca4444c273a82544
daumpark <ekdna1120@inha.edu>
2024-08-07 18:16:26 +0900

    Upgrade detection model to YOLOv8

--- scripts/depth_combination.py
+++ scripts/depth_combination.py
@@ -17,48 +17,59 @@
 
 import rospy
 import pcl
-from darknet_ros_msgs.msg import BoundingBoxes
 from sensor_msgs.msg import PointCloud2
-import sensor_msgs.point_cloud2 as pc2
 import ros_numpy
 import numpy as np
 import math
 import tf
+from sensor_msgs.msg import Image
+from ultralytics import YOLO
 
-height = 720
-width = 1280
+height = 480
+width = 640
 if_pcl_ready = 0
 parent_frame = "camera_color_optical_frame"
+detection_model = YOLO("yolov8m.pt")
+det_image_pub = rospy.Publisher("/ultralytics/detection/image", Image, queue_size=5)
+class_names = detection_model.names
 
-def darknet_callback(data):
+def image_callback(data):
     global p,if_pcl_ready
 
-    obj_tf = tf.TransformBroadcaster()
-    if(if_pcl_ready):
-        bounding_boxes = data.bounding_boxes
-        id = 0
-        for i in bounding_boxes:
-            x = 0
-            y = 0
-            z = 0
-            valid_num = 0
-            length = i.xmax - i.xmin
-            height = i.ymax - i.ymin
-            for ix in range(int(i.xmin+length/4),int(i.xmax-length/2)):
-                for iy in range(int(i.ymin+height/4),int(i.ymax-height/4)):
-                    index = int((iy-1)*width+ix)
-                    position = p[index]
-                    if not(math.isnan(position[2])):
-                        x = x + position[0]
-                        y = y + position[1]
-                        z = z + position[2]
-                        valid_num = valid_num + 1
-            if(valid_num):
-                x = x/valid_num
-                y = y/valid_num
-                z = z/valid_num
-            obj_tf.sendTransform((x, y, z),tf.transformations.quaternion_from_euler(0, -math.pi/2, math.pi/2),rospy.Time.now(),i.Class+str(id),parent_frame)
-            id = id + 1
+    array = ros_numpy.numpify(data)
+    if det_image_pub.get_num_connections():
+        det_result = detection_model(array)
+        det_annotated = det_result[0].plot(show=False)
+        det_image_pub.publish(ros_numpy.msgify(Image, det_annotated, encoding="rgb8"))
+        obj_tf = tf.TransformBroadcaster()
+        if(if_pcl_ready):
+            bounding_boxes = det_result[0].boxes
+            id = 0
+            for i in bounding_boxes:
+                x = 0
+                y = 0
+                z = 0
+                valid_num = 0
+                xmin, ymin, xmax, ymax = i.xyxy[0]
+                class_id = int(i.cls)
+                class_name = class_names[class_id]
+                length = xmax - xmin
+                height = ymax - ymin
+                for ix in range(int(xmin+length/4),int(xmax-length/2)):
+                    for iy in range(int(ymin+height/4),int(ymax-height/4)):
+                        index = int((iy-1)*width+ix)
+                        position = p[index]
+                        if not(math.isnan(position[2])):
+                            x = x + position[0]
+                            y = y + position[1]
+                            z = z + position[2]
+                            valid_num = valid_num + 1
+                if(valid_num):
+                    x = x/valid_num
+                    y = y/valid_num
+                    z = z/valid_num
+                obj_tf.sendTransform((x, y, z),tf.transformations.quaternion_from_euler(0, -math.pi/2, math.pi/2),rospy.Time.now(),class_name+str(id),parent_frame)
+                id = id + 1
 
 def depth_callback(data):
     global p,if_pcl_ready
@@ -72,8 +83,8 @@ def depth_callback(data):
 
 def listener():
     rospy.init_node('depth_combination', anonymous=True)
-    rospy.Subscriber("/darknet_ros/bounding_boxes", BoundingBoxes, darknet_callback)
     rospy.Subscriber("/camera/depth_registered/points", PointCloud2, depth_callback)
+    rospy.Subscriber("/camera/color/image_rect_color", Image, image_callback)
     rospy.spin()
 
 if __name__ == '__main__':
